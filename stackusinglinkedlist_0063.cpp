1652a30c0574b5f52a7668bcb5e3d11b67ac26df
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-10 00:07:40 -0700

    masukan class stack

--- stackusinglinkedlist_0063.cpp
+++ stackusinglinkedlist_0063.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// Node class reperesenting a single node in the linked list
 class Node {
-public;
+public:
 	int data;
 	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
+
+// Stack class
+class stack {
+private:
+	Node* top; // pointer to the node of the stack
+
+
 };
