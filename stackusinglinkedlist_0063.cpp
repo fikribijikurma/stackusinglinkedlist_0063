95d145f48f6599e89bc3003ad678996c85f7bc4c
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-10 00:36:30 -0700

    tampilkan boolean

--- stackusinglinkedlist_0063.cpp
+++ stackusinglinkedlist_0063.cpp
@@ -54,7 +54,12 @@ public:
 				cout << current->data << " " << endl;
 				current = current->next;
 			}
-		}
+			cout << endl;
+		} // Return the value of the top node
+	}
+
+	// isEmpty operation: check if the stack is empty
+	bool isEmpty() {
 
 	}
 };
