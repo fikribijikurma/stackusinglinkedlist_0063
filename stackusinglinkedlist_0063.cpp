f1003c3f8063dea732981e238021b995e71b9a4c
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-10 00:31:12 -0700

    masukan while

--- stackusinglinkedlist_0063.cpp
+++ stackusinglinkedlist_0063.cpp
@@ -28,15 +28,31 @@ public:
 		newNode->data = value; // 2. assign value
 		newNode->next = top; // 3. set the next pointer of the new nofde to the current top node
 		top = newNode; // 4. Update the top pointer to the new node
-		cout << "push value : " << endl;
+		cout << "push value : " << value << endl;
 		return value;
 
 	}
 
 	// pop operation: remove the topmost element from the stack
 	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty. " << endl;
+		}
+		cout << "popped value: " << top->data << endl;
+		top = top->next; // update the top pointer to the next node
+
+	}
+
+	// peek/Top operation: Retrieve thr value of the topmost element without removing it
+	void peek() {
 		if (top == NULL) {
 			cout << "List is empty. " << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+
+			}
 		}
 
 	}
