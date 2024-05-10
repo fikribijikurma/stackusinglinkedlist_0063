2aa5f1ee535a734290d9c07b23625fa98403e385
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-10 00:15:30 -0700

    memasukan void

--- stackusinglinkedlist_0063.cpp
+++ stackusinglinkedlist_0063.cpp
@@ -17,5 +17,15 @@ class stack {
 private:
 	Node* top; // pointer to the node of the stack
 
+public:
+	stack() {
+		top = NULL; // initialize the stack with a null top pointer
+	}
 
+	// push operation: Insert an element onto the top of the stack
+	int push(int value) {
+		Node* newNode = new Node(); // 1. Allocate memory for the new node
+		newNode->data = value; // 2. assign value
+
+	}
 };
